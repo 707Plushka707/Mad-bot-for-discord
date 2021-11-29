42c8b020bafd763ebbe372a201fc6324aa5892c6
windows <nukool.li@inet.co.th>
2021-11-29 16:32:12 +0700

    add http server 5k

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const { Client, Intents } = require('discord.js');
-const dotenv  = require('dotenv');
+const { Client, Intents } = require("discord.js");
+const dotenv = require("dotenv");
+const http = require("http");
+http.createServer(function (request, response) {
+  dotenv.config();
 
-dotenv.config();
-
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
-    ]
-});
-client.on('ready', () => {
-    console.log('ready ON');
-});
-client.on('messageCreate', msg => {
-    const textArr = ['ควย', 'เหี้ย', 'ทำเหี้ย', 'ทำส้นตีน'];
+  const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  });
+  client.on("ready", () => {
+    console.log("Server Running ON Port 5000");
+  });
+  client.on("messageCreate", (msg) => {
+    const textArr = ["ควย", "เหี้ย", "ทำเหี้ย", "ทำส้นตีน"];
     const text = msg.content;
     let RandomNumber = Math.floor(Math.random() * 4);
     let HaveDot = text.search(/[.]/g);
-    if ((HaveDot >= 0) || text == 'จุด') {
-        msg.react('🖕');
-        msg.reply(`จุด${textArr[RandomNumber]}ไร <@${msg.author.id}>`);
+    if (HaveDot >= 0 || text == "จุด") {
+      msg.react("🖕");
+      msg.reply(`จุด${textArr[RandomNumber]}ไร <@${msg.author.id}>`);
     }
-})
-client.login(process.env.TOKEN);
+  });
+  client.login(process.env.TOKEN);
+}).listen(process.env.PORT || 5000)
