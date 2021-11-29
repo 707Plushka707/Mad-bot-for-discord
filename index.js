519e65e99cae7a9f843413979a9ade47e999d05d
windows <nukool.li@inet.co.th>
2021-11-29 16:16:00 +0700

    heroku

--- index.js
+++ index.js
@@ -22,4 +22,4 @@ client.on('messageCreate', msg => {
         msg.reply(`จุด${textArr[RandomNumber]}ไร <@${msg.author.id}>`);
     }
 })
-client.login(secrets.TOKEN);
+client.login(process.env.TOKEN);
