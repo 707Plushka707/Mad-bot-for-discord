57ddd25a1cb93fddf4185bed018891537f5cf9fe
Anukul Thienkasemsuk <anukul.th@protonmail.com>
2021-12-03 02:06:27 +0700

    dont need params

--- zenquotes.js
+++ zenquotes.js
@@ -2,12 +2,19 @@ import axios from "axios";
 const { get } = axios;
 import { Quote } from "./db.js";
 
+/**
+ * Method: Get
+ * Response:
+ * {
+ *  a: string,
+ *  h: string,
+ *  q: string
+ * }
+ */
 const zenUrlRandom = "https://zenquotes.io/api/random/";
 
-const zenGetRandom = async (params) => {
-  const ramdomQuote = await get(zenUrlRandom, {
-    params: params,
-  });
+const zenGetRandom = async () => {
+  const ramdomQuote = await get(zenUrlRandom);
   await Quote.create(ramdomQuote.data[0]);
   return ramdomQuote.data[0];
 };
